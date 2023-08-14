f1d0bbff1900ef7aaf89c7a5cc8dc3bd0b255d44
Muskan-Sandhay <134076761+Muskan-Sandhay@users.noreply.github.com>
2023-08-14 19:16:21 -0400

    Add files via upload

---
+++ Data/BookMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BookMVC.Models;
+
+namespace BookMVC.Data
+{
+    public class BookMVCContext : DbContext
+    {
+        public BookMVCContext (DbContextOptions<BookMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookMVC.Models.Books> Books { get; set; } = default!;
+    }
+}
---
+++ Models/Books.cs
@@ -0,0 +1,11 @@
+﻿namespace BookMVC.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Rating { get; set; }
+      public string Price { get; set; }
+    }
+}
